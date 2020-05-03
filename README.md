fe23906922a438ddbab0c8d91662c45e7a7f5919
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2020-05-03 12:20:32 -0400

    Add Configuration

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prettier Config
+
+My personal prettier configuration for ease of use and portability.
---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    htmlWhitespaceSensitivity: 'css',
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    printWidth: 120,
+    proseWrap: 'preserve',
+    quoteProps: 'consistent',
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'none',
+    useTabs: false
+};
